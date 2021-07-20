aa8befcd85daf774c9cabfd99f6820218d2a2d89
Gabriel <gabrielperren@gmail.com>
2021-07-20 17:02:21 -0300

    updt Gaia EDR3 filters

--- modules/HARDCODED.py
+++ modules/HARDCODED.py
@@ -2,13 +2,11 @@
 #
 # Only Gaia EDR3 and Gaia DR2 photometric systems are supported
 cmd_systs = {
-    'gaiadr2': (
-        ('Gmag', 'G_BPmag', 'G_RPmag'), (6437.7, 5309.57, 7709.85)),
     'gaiaedr3': (
-        ('Gmag', 'G_BPmag', 'G_RPmag'), (6422.01, 5335.42, 7739.17))
+        ('Gmag', 'G_BPmag', 'G_RPmag'), (6390.21, 5182.58, 7825.08))
 }
 # line where the header starts in the CMD isochrone files
-idx_header = 11
+idx_header = 12
 # Column names in isochrone files
 logAge_name, label_name = 'logAge', 'label'
 # Column names
